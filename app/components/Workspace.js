a0b2919187336dbaf28678bebf77c2a9ed2b0e07
Evgenii Loikov <eloikov@chimera-entertainment.de>
2019-03-13 22:28:27 +0100

    Adding simple schema

--- app/components/Workspace.js
+++ app/components/Workspace.js
@@ -1,17 +1,31 @@
 // @flow
 import React, { Component } from 'react';
 import styles from './Workspace.css';
+import Form from "react-jsonschema-form";
 
 type Props = {};
+
+const schema = {
+  title: "Todo",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {type: "string", title: "Title", default: "A new task"},
+    done: {type: "boolean", title: "Done?", default: false}
+  }
+};
+
+const log = (type) => console.log.bind(console, type);
 
 export default class Workspace extends Component<Props> {
   props: Props;
 
   render() {
     return (
-      <div className={styles.container} data-tid="container">
-        <h2>Workspace</h2>
-      </div>
+      <Form schema={schema}
+            onChange={log("changed")}
+            onSubmit={log("submitted")}
+            onError={log("errors")} />
     );
   }
 }
