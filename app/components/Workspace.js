d0f493a0b3daef9afab7cfd8667ed209d76ee0c3
Evgenii Loikov <eloikov@chimera-entertainment.de>
2019-03-14 09:48:37 +0100

    Template

--- app/components/Workspace.js
+++ app/components/Workspace.js
@@ -1,31 +1,38 @@
 // @flow
 import React, { Component } from 'react';
-import styles from './Workspace.css';
-import Form from "react-jsonschema-form";
+// import styles from './Workspace.css';
+import Form from 'react-jsonschema-form';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-solarized/bootstrap-solarized-light.css';
 
 type Props = {};
 
 const schema = {
-  title: "Todo",
-  type: "object",
-  required: ["title"],
+  title: 'Todo',
+  type: 'object',
+  required: ['title'],
   properties: {
-    title: {type: "string", title: "Title", default: "A new task"},
-    done: {type: "boolean", title: "Done?", default: false}
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false }
   }
 };
 
-const log = (type) => console.log.bind(console, type);
+const log = type => console.log.bind(console, type);
 
 export default class Workspace extends Component<Props> {
   props: Props;
 
   render() {
     return (
-      <Form schema={schema}
-            onChange={log("changed")}
-            onSubmit={log("submitted")}
-            onError={log("errors")} />
+      <div>
+        <Form
+          schema={schema}
+          onChange={log('changed')}
+          onSubmit={log('submitted')}
+          onError={log('errors')}
+        />
+      </div>
     );
   }
 }
