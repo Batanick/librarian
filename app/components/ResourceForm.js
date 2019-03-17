40a954a7385dc7e3fd181bca94332f0993c0edb0
Evgenii Loikov <eloikov@chimera-entertainment.de>
2019-03-17 17:21:09 +0100

    Dndsupport

--- app/components/ResourceForm.js
+++ app/components/ResourceForm.js
@@ -1,14 +1,18 @@
 // @flow
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Form from 'react-jsonschema-form';
+import PropTypes from 'prop-types';
 
-import {DragSource} from "react-dnd";
-
-type Props = {
-  id: ""
-};
+import { DragSource } from 'react-dnd';
 
 import styles from 'bootswatch/dist/darkly/bootstrap.min.css';
+
+type Props = {
+  connectDragSource: PropTypes.object,
+  isDragging: PropTypes.object,
+  left: 0,
+  top: 0
+};
 
 const style = {
   position: 'absolute'
@@ -19,8 +23,8 @@ const schema = {
   type: 'object',
   required: ['title'],
   properties: {
-    title: {type: 'string', title: 'Title', default: 'A new task'},
-    done: {type: 'boolean', title: 'Done?', default: false}
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false }
   }
 };
 
@@ -28,88 +32,42 @@ const log = type => console.log.bind(console, type);
 
 const formSource = {
   beginDrag(props) {
-    // console.log(props)
-    return {id: props.id};
-  },
-  endDrag(props, monitor, component) {
-    // console.log(monitor.getDifferenceFromInitialOffset());
-    if (!component) {
-      return
-    }
-
-    const delta = monitor.getDifferenceFromInitialOffset();
-    // const left = Math.round(item.left + delta.x);
-    // const top = Math.round(item.top + delta.y);
-    // component.moveBox(left, top)
+    return { id: props.id, left: props.left, top: props.top };
   }
 };
 
 function collect(connect, monitor) {
-  // console.log(monitor.didDrop());
-
   return {
     connectDragSource: connect.dragSource(),
     connectDragPreview: connect.dragPreview(),
-    isDragging: monitor.isDragging(),
-  }
+    isDragging: monitor.isDragging()
+  };
 }
 
 class ResourceForm extends Component<Props> {
-
-  constructor(props) {
-    super(props);
-    this.state = {pos: {left: 42, top: 20}};
-  }
-
-  state: {
-    pos: {
-      left: 10,
-      top: 10
-    }
-  };
-
   render() {
-    const {
-      connectDragSource,
-      isDragging
-    } = this.props;
-
-    const left = this.state.pos.left;
-    const top = this.state.pos.top;
+    const { connectDragSource, isDragging, left, top } = this.props;
 
     if (isDragging) {
-      return null
+      return null;
     }
 
-    console.log(this.state);
-
     return connectDragSource(
-      <div style={Object.assign({}, style, {left, top})}>
+      <div style={Object.assign({}, style, { left, top })}>
         <div className={styles.card}>
           <div className={styles['card-header']}>CardConfig</div>
           <div className={styles['card-body']}>
             <Form
               schema={schema}
-              // onChange={log('changed')}
-              // onSubmit={log('submitted')}
-              // onError={log('errors')}
+              onChange={log('changed')}
+              onSubmit={log('submitted')}
+              onError={log('errors')}
             />
           </div>
         </div>
-
       </div>
-    )
+    );
   }
-
-  // movePos(left, top) {
-  //   console.log("Drop:" + left);
-  //   this.setState({
-  //     pos: {
-  //       left: left,
-  //       top: top
-  //     }
-  //   });
-  // }
 }
 
-export default DragSource("resource", formSource, collect)(ResourceForm)
+export default DragSource('resource', formSource, collect)(ResourceForm);
--- app/components/Workspace.js
+++ app/components/Workspace.js
@@ -1,19 +1,19 @@
 // @flow
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { DropTarget } from 'react-dnd';
+import PropTypes from 'prop-types';
+import update from 'immutability-helper';
 
 import ResourceForm from './ResourceForm';
-import {DropTarget} from 'react-dnd';
 
 type Props = {
-  pos: {
-    left: 0,
-    top: 0
-  }
+  connectDropTarget: PropTypes.object
 };
+
 const styles = {
   flex: 1,
   flexDirection: 'column',
-  position: 'relative',
+  position: 'relative'
 };
 
 function collect(connect, monitor) {
@@ -23,34 +23,53 @@ function collect(connect, monitor) {
     connectDropTarget: connect.dropTarget(),
     // You can ask the monitor about the current drag state:
     isOver: monitor.isOver(),
-    isOverCurrent: monitor.isOver({shallow: true}),
+    isOverCurrent: monitor.isOver({ shallow: true }),
     canDrop: monitor.canDrop(),
     itemType: monitor.getItemType(),
     getDifferenceFromInitialOffset: monitor.getDifferenceFromInitialOffset()
-  }
+  };
 }
 
 const target = {
   drop(props, monitor, component) {
-
     const item = monitor.getItem();
     const delta = monitor.getDifferenceFromInitialOffset();
     const left = Math.round(item.left + delta.x);
     const top = Math.round(item.top + delta.y);
 
-    component.moveChild(item.id, delta.x, delta.y)
+    component.moveChild(item.id, left, top);
   }
-
 };
 
 class Workspace extends Component<Props> {
   props: Props;
 
+  constructor(...args) {
+    super(args);
+    this.state = {
+      elements: {
+        ResourceId: { top: 20, left: 80, title: 'Drag me around' },
+        ResourceId2: { top: 40, left: 40, title: 'Drag me around' }
+      }
+    };
+  }
+
+  moveChild(id, left, top) {
+    this.setState(prevState =>
+      update(prevState, {
+        elements: {
+          [id]: {
+            $merge: { left, top }
+          }
+        }
+      })
+    );
+  }
+
   render() {
-    const {connectDropTarget} = this.props;
-
-    return connectDropTarget((
-
+    const { connectDropTarget } = this.props;
+    const { elements } = this.state;
+    return connectDropTarget(
       <div id="workspace" style={Object.assign({}, styles)}>
         <h3>Workspace</h3>
         <h3>Workspace</h3>
@@ -58,14 +77,22 @@ class Workspace extends Component<Props> {
         <h3>Workspace</h3>
         <h3>Workspace</h3>
         <h3>Workspace</h3>
-        <ResourceForm id={"RandomResourceId"}/>
+        {Object.keys(elements).map(key => {
+          const { left, top } = elements[key];
+          return (
+            <ResourceForm
+              key={key}
+              id={key}
+              left={left}
+              top={top}
+              connectDragSource=""
+              isDragging="false"
+            />
+          );
+        })}
       </div>
-    ));
-  }
-
-  moveChild(id, left, top) {
-    console.log({id: id, left: left, top: top});
+    );
   }
 }
 
-export default DropTarget("resource", target, collect)(Workspace);
+export default DropTarget('resource', target, collect)(Workspace);
